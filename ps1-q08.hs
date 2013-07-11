615f86acf2c4f891bcb32cb09bcc8570d589e300
Lukas Toth <lukas@lukastoth.com>
2013-07-11 11:59:30 -0400

    Remove unhex' helper fn

--- ps1-q08.hs
+++ ps1-q08.hs
@@ -1,18 +1,14 @@
 import Data.Char
 import Data.Hex
 import Data.Bits
+import Control.Monad
 
 main = putStrLn $ encrypt k "attack at dusk"
 
 k = encrypt "09e1c5f70a65ac519458e7e53f36" "attack at dawn"
 
 encrypt :: String -> String -> String
-encrypt k m = map toLower $ hex $ map chr $ encrypt' (map ord m) (map ord $ unhex' k)
+encrypt k m = map toLower $ hex $ map chr $ encrypt' (map ord m) (map ord $ join $ unhex k)
 
 encrypt' :: [Int] -> [Int] -> [Int]
 encrypt' k m = zipWith xor m k
-
-unhex' :: (Monad m, Hex (m b)) => m b -> m b
-unhex' h = do
-    x <- unhex h
-    x
