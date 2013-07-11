89168d6eeebfbfc7a691e808f7f502fbdf460f57
Lukas Toth <lukas@lukastoth.com>
2013-07-11 12:01:04 -0400

    Switch order of arguments

--- ps1-q08.hs
+++ ps1-q08.hs
@@ -8,7 +8,7 @@ main = putStrLn $ encrypt k "attack at dusk"
 k = encrypt "09e1c5f70a65ac519458e7e53f36" "attack at dawn"
 
 encrypt :: String -> String -> String
-encrypt k m = map toLower $ hex $ map chr $ encrypt' (map ord m) (map ord $ join $ unhex k)
+encrypt k m = map toLower $ hex $ map chr $ encrypt' (map ord $ join $ unhex k) (map ord m)
 
 encrypt' :: [Int] -> [Int] -> [Int]
 encrypt' k m = zipWith xor m k
