eaba9c07115033745cae749179a4083552af2ee6
Lukas Toth <lukas@lukastoth.com>
2013-07-21 23:43:06 -0400

    First completely working version of ps3-q01

--- ps3-q01.hs
+++ ps3-q01.hs
@@ -1,31 +1,33 @@
 {-# LANGUAGE ViewPatterns #-}  
 import Data.Int
+import Data.Char
 import Data.Hex
 import System.Environment
 import Crypto.Hash.SHA256
-import qualified Data.ByteString as S
-import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Char8 as S
 
 blockSize = 1024
 
---main = do
---  (fileName:_) <- getArgs
---  h0 <- recursiveHash fileName
---  putStrLn $ hex h0
+main = do
+  (fileName:_) <- getArgs
+  h0 <- recursiveHash fileName
+  putStrLn $ toHex h0
 
 recursiveHash f = do
-  contents <- B.readFile f
-  return $ foldr recursiveHash' S.empty $ B.toChunks contents
+  contents <- S.readFile f
+  return $ recursiveHash' contents S.empty
 
 recursiveHash' c h =
   let bs = splitEvery blockSize c
   in  foldr (\x acc -> hash $ S.append x acc) h bs
     
+toHex b = map toLower $ hex $ S.unpack b
+
 -- based on chunk :: Int -> [a] -> [[a]] from
 -- http://www.haskell.org/haskellwiki/Data.List.Split
 
 -- | split at regular intervals
 splitEvery :: Int -> S.ByteString -> [S.ByteString]
-splitEvery _ (S.uncons -> Nothing) = [S.empty]
+splitEvery _ (S.uncons -> Nothing) = []
 splitEvery n xs = y1 : splitEvery n y2
   where (y1, y2) = S.splitAt n xs
